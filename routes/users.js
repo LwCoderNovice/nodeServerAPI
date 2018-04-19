c95171bed7184ba6073c48202b6a97e768048a6b
apple <apple@appledeMacBook-Pro.local>
2018-04-20 00:38:39 +0800

    update url retrun

--- routes/users.js
+++ routes/users.js
@@ -3,12 +3,15 @@ var router = express.Router();
 var userFunc = require('../function/userFunc');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.send('respond with a resource');
 });
 
-router.post('/register', function(req, res, next) {
-  userFunc.add(req, res, next);
+router.post('/register', (req, res, next) => {
+    
+  if(userFunc.add(req, res, next)) {
+    res.render('/');
+  }
 })
 
 module.exports = router;
